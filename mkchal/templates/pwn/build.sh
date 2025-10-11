755a3441ed4d62b0cf564dd2f148b8b62ef60c63
Jack Roscoe <jack.x.roscoe@gmail.com>
2025-10-11 00:21:21 -0400

    fixed pwn build system

--- mkchal/templates/pwn/build.sh
+++ mkchal/templates/pwn/build.sh
@@ -3,7 +3,7 @@
 # builds your challenge
 # can be used locally for testing, but is used inside build container
 
-cd $(dirname $0)
+cd "$(dirname $0)"
 
 # by default Dockerfile_build copies chall out to dist
 # if multiple build files are desired, copy those as well
--- mkchal/templates/pwn/build_dist.sh
+++ mkchal/templates/pwn/build_dist.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-cd $(dirname $0)
+cd "$(dirname $0)"
+
+mkdir -p build_out
+
+# pass path of libc and linker so container knows where to copy out from
+# configure these if you use arm and it needs to change
+LIB_PATH="/srv/lib/x86_64-linux-gnu"
+export LIBC_PATH="$LIB_PATH/libc.so.6"
+export LINKER_PATH="$LIB_PATH/ld-linux-x86-64.so.2"
 
 # pass user id and group id we want chall build file to have to docker-compose
 export USER_ID=$(id -u)
@@ -11,4 +19,6 @@ export CHALL_HASH='{hash}' # please include this envar in your final build
 # If you need to use sudo, you have to pass options to sudo to make sure
 # USER_ID and GROUP_ID env variables are passed into docker-compose
 # Otherwise outputed files in dist will be owned by root
-cd deploy && sudo -E docker-compose up --build build
+cd deploy \
+    && sudo -E docker-compose up --build build \
+    && sudo -E docker-compose up --build libc
